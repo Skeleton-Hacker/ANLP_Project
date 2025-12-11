668db5b8adb35f9d5aaa5a5218897455ebf6d27c
George Rahul <georgerahul24@gmail.com>
2025-12-11 09:57:46 +0530

    Lets Start Over. Also, added sbatch script to run it

---
+++ pipeline_2/run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+#SBATCH --gres=gpu:4
+#SBATCH -A research
+#SBATCH --cpus-per-gpu=9
+#SBATCH --mem=96G
+#SBATCH --time=4-00:00:00
+#SBATCH --output=job_output.txt
+#SBATCH --mail-user=george.rahul@research.iiit.ac.in
+#SBATCH --mail-type=ALL
+
+echo "=========================================="
+echo "SLURM_JOB_ID = $SLURM_JOB_ID"
+echo "SLURM_NODELIST = $SLURM_NODELIST"
+echo "SLURM_JOB_GPUS = $SLURM_JOB_GPUS"
+echo "=========================================="
+
+# ----------------------------------------------------------
+# Correct micromamba setup for NON-INTERACTIVE SLURM BASH
+# ----------------------------------------------------------
+export MAMBA_ROOT_PREFIX=$HOME/mamba
+export PATH="$MAMBA_ROOT_PREFIX/bin:$PATH"
+
+# The IMPORTANT line: initialize bash micromamba hook
+eval "$(micromamba shell hook --shell bash)"
+
+# now activate will work
+micromamba activate next-level-lm
+
+echo "Micromamba environment activated!"
+python --version
+
+# ----------------------------------------------------------
+# Create scratch directory (safe)
+# ----------------------------------------------------------
+mkdir -p -m 700 /ssd_scratch/gr
+mkdir -p /ssd_scratch/gr/cache
+
+# ----------------------------------------------------------
+# Navigate to your project
+# ----------------------------------------------------------
+cd /home2/gr/ANLP_Project/pipeline_2
+# ----------------------------------------------------------
+# RUN YOUR CODE
+# ----------------------------------------------------------
+accelerate launch --num_processes 4 semantic_chunking.py
+accelerate launch --num_processes 4 chunk_embeddings.py
+
